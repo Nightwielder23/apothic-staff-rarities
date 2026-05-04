dce342e466c5263f52b56d9bfc89c0eeede3186d
Nightwielder23 <nightwielder23@users.noreply.github.com>
2026-05-04 00:33:07 -0700

    initial setup

---
+++ src/main/java/com/nightwielder/apothicstaffrarities/ApothicStaffRarities.java
@@ -0,0 +1,15 @@
+package com.nightwielder.apothicstaffrarities;
+
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+@Mod(ApothicStaffRarities.MODID)
+public class ApothicStaffRarities {
+    public static final String MODID = "apothic_staff_rarities";
+    public static final Logger LOGGER = LogUtils.getLogger();
+
+    public ApothicStaffRarities(FMLJavaModLoadingContext context) {
+    }
+}
